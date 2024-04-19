7fee811b420b83fd85a3429e05dab49945e27f8d
Boso <linboso0@gmail.com>
2024-04-19 08:54:16 +0800

    tset build
    1. 測試 github.io 靜態網頁

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/home/csl/Desktop/CityScience-TaipeiTech.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/home/csl/Desktop/CityScience-TaipeiTech.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/home/csl/Desktop/CityScience-TaipeiTech.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/home/csl/Desktop/CityScience-TaipeiTech.github.io/src/css/custom.css"),
+];
